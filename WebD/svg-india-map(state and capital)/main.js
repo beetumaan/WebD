4e4c7e27d35d5ccfc9402ebc4243fe90347ec05b
Beetu Maan <69500855+beetumaan@users.noreply.github.com>
2021-05-10 00:17:01 +0530

    Update main.js

--- WebD/svg-india-map(state and capital)/main.js
+++ WebD/svg-india-map(state and capital)/main.js
@@ -1,9 +1,9 @@
-$("path, circle").hover(function(e) {
+$("path").hover(function(e) {
   $('#info-box').css('display','block');
   $('#info-box').html($(this).data('info'));
 });
 
-$("path, circle").mouseleave(function(e) {
+$("path").mouseleave(function(e) {
   $('#info-box').css('display','none');
 });
 
@@ -11,12 +11,3 @@ $(document).mousemove(function(e) {
   $('#info-box').css('top',e.pageY-$('#info-box').height()-30);
   $('#info-box').css('left',e.pageX-($('#info-box').width())/2);
 }).mouseover();
-
-var ios = /iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream;
-if(ios) {
-  $('a').on('click touchend', function() {
-    var link = $(this).attr('href');
-    window.open(link,'_blank');
-    return false;
-  });
-}
